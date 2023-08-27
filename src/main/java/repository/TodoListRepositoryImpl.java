ee4f3c2e31579e349e3262b882bb37cb4f74ae44
hadipsy27 <hadipsy27@gmail.com>
2023-08-27 18:02:35 +0700

    Repository : Menghapus Todolist

--- src/main/java/repository/TodoListRepositoryImpl.java
+++ src/main/java/repository/TodoListRepositoryImpl.java
@@ -5,6 +5,7 @@ import entity.Todolist;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class TodoListRepositoryImpl implements TodoListRepository{
@@ -62,21 +63,39 @@ public class TodoListRepositoryImpl implements TodoListRepository{
         }
     }
 
-    @Override
-    public boolean remove(Integer number) {
-        if((number - 1) >= data.length){
-            return false;
-        } else if (data[number - 1] == null) {
-            return false;
-        } else {
-            for (int i = (number - 1); i  < data.length; i++) {
-                if(i == (data.length - 1)){
-                    data[i] = null;
+    private boolean isExist(Integer number){
+        String sql = "SELECT id FROM todolist WHERE id = ?";
+        try(Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+
+            preparedStatement.setInt(1, number);
+            try(ResultSet resultSet = preparedStatement.executeQuery()){
+                if (resultSet.next()) {
+                    return true;
                 } else {
-                    data[i] = data[i + 1];
+                    return false;
                 }
             }
-            return true;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean remove(Integer number) {
+        if (isExist(number)) {
+            String sql = "DELETE FROM todolist WHERE id = ?";
+            try(Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setInt(1, number);
+                preparedStatement.executeUpdate();
+
+                return true;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return false;
         }
     }
 }
